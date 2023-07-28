6daec00365c727f066f1c80c37521e0c98b1941a
Rishabh Amga <rishabhamga@lambdatest.com>
2023-07-28 20:38:23 +0530

    my portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,343 @@
+// Contains constant data for using in website
+// ! Don't remove anything from here if not sure
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  threejs,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+} from "../assets";
+
+// Navbar Links
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "source-code",
+    title: "Source Code",
+    link: "http://www.github.com/Technical-Rishabh-tech/3d-portfolio/",
+  },
+];
+
+// Services
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+// Technologies
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "OpenAI",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+// Experiences
+const experiences = [
+  {
+    title: "Solving Problems, One Web Solution at a Time",
+    company_name: "",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "",
+    points: [
+      "One of the most rewarding aspects of being a software engineer is the opportunity to solve real-life problems for customers. I am deeply passionate about using my skills and expertise to build web solutions that address the pain points of customers and make a positive impact in their lives. ",
+    ],
+  },
+  {
+    title: "The Magic of Generative AI",
+    company_name: "",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "",
+    points: [
+      "Generative AI is where the enchantment begins. Witnessing machines create content, art, and human-like responses is nothing short of magical. My fascination with Generative AI knows no bounds, and I am constantly exploring its boundless possibilities. With a diverse set of technologies at my disposal, I've mastered the art of utilizing OpenAI API, LLM, natural language processing, and prompt engineering.",
+     
+    ],
+  },
+  {
+    title: "Fueling Innovation through Collaboration",
+    company_name: "",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "",
+    points: [
+      "Innovation thrives in collaboration. I find immense joy in working on team-based projects, where creativity knows no bounds. Together, we conquer complex challenges and bring groundbreaking solutions to life. it's about making a difference in people's lives. I prioritize the user in every  product I build. By focusing on user needs and experiences, I ensure that my solutions are not only advanced but also user-friendly and impactful.",
+  
+    ],
+  },
+  {
+    title: "Journey from SAAS to AI Adventures",
+    company_name: "Shopify",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Currently, I'm a member of the technical staff at a SAAS (Software as a Service) company, where I've contributed to exciting projects. However, my true adventure lies in the exploration of Generative AI. Each day, I'm on a mission to push the boundaries of what AI can achieve. Passion for learning drives me forward. Taking on new challenges beyond my comfort zone fuels my growth as a software engineer. Constantly learning keeps me at the forefront of the AI revolution.",
+    ],
+  },
+];
+
+// Testimonials
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+// Projects
+const projects = [
+  {
+    name: "Disney+ Clone",
+    description:
+      "Disney+ is one of the biggest streaming platforms used by millions of people world-wide and allows us to stream high quality content in 4k and various other formats",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project1,
+    source_code_link: "https://github.com/Technical-Rishabh-tech/disney-clone",
+    live_site_link: "https://clonedisneyplus.web.app/",
+  },
+  {
+    name: "Golds Gym",
+    description:
+      "Web application that enables users to search for fitness exercises, effective personalized positions, and recommends new exercises based on their personal preferences.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project2,
+    source_code_link: "https://github.com/Technical-Rishabh-tech/fitness-app",
+    live_site_link: "https://fitness-gym-react.netlify.app/",
+  },
+  {
+    name: "Shoppy",
+    description:
+      "The most personalised admin dashboard web application that allows enables users to choose customized themes and dark mode with different pages and variety of charts.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "syncfusion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project3,
+    source_code_link:
+      "https://github.com/Technical-Rishabh-tech/admin-dashboard",
+    live_site_link: "https://shoppy-dashboard-react.netlify.app/",
+  },
+  {
+    name: "TikTok Clone",
+    description:
+      "A Next JS Web Application that enables users to upload videos of any length and size, create accounts and connect with other people just like any other social media.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project4,
+    source_code_link: "https://github.com/Technical-Rishabh-tech/tiktok-clone",
+    live_site_link: "https://tiktok-clone-react.vercel.app/",
+  },
+  {
+    name: "Cryptoverse",
+    description:
+      "Best Cryptocurrency web application that allows users to view price, market cap and daily change in realtime for almost every cryptocurrency in the world.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project5,
+    source_code_link: "https://github.com/Technical-Rishabh-tech/crypto-app",
+    live_site_link: "https://reactjscryptoapp.netlify.app/",
+  },
+  {
+    name: "Travel Advisor",
+    description:
+      "Web application that enables you to view your nearby restaurants, hotels and attractions which can be sorted by ratings, price and much more.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "rapidapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "material-ui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: project6,
+    source_code_link:
+      "https://github.com/Technical-Rishabh-tech/travel-advisor",
+    live_site_link: "https://travel-advisor-reactjs.netlify.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
