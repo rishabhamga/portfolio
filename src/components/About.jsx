c8ffe76b7832d350dae64d75a8784e63be8d2efc
Rishabh amga <rishabhamga17@gmail.com>
2024-01-04 01:36:48 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
+
+//
+
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
